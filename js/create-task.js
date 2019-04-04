05e18f58737e767ff148deedc1629841fbb1a43d
Sofia Diaz <sdiaz1@ucvts.tec.nj.us>
2019-04-04 10:52:45 -0400

    organized results

--- js/create-task.js
+++ js/create-task.js
@@ -26,38 +26,95 @@ function showCareerButton() {
   }
 }
 
-function itButton() {
-  show("compSciResultOne", "compSciResultTwo")
-  
+function compSciButton() {
+  showCompSciResults()
+
   //should go to an it result
 }
 
+function showCompSciResults() {
+  if (document.getElementById("compSciResultOne").style.visibility == "visible" &&
+      document.getElementById("compSciResultTwo").style.visibility == "visible") {
+
+      document.getElementById("compSciResultOne").style.visibility="hidden";
+      document.getElementById("compSciResultTwo").style.visibility="hidden";
+  } else if (document.getElementById("compSciResultOne").style.visibility == "visible") {
+    document.getElementById("compSciResultTwo").style.visibility="visible";
+  } else if (document.getElementById("compSciResultOne").style.visibility != "visible") {
+    document.getElementById("compSciResultOne").style.visibility="visible";
+  }
+}
+
 function govButton() {
-  show("govResultOne", "govResultTwo")
-  
+  showGovResults()
+
   //should go to a gov result
 }
 
+function showGovResults() {
+  if (document.getElementById("govResultOne").style.visibility == "visible" &&
+      document.getElementById("govResultTwo").style.visibility == "visible") {
+
+      document.getElementById("govResultOne").style.visibility="hidden";
+      document.getElementById("govResultTwo").style.visibility="hidden";
+  } else if (document.getElementById("govResultOne").style.visibility == "visible") {
+    document.getElementById("govResultTwo").style.visibility="visible";
+  } else if (document.getElementById("govResultOne").style.visibility != "visible") {
+    document.getElementById("govResultOne").style.visibility="visible";
+  }
+}
+
 function businessButton() {
-  show("businessResultOne", "businessResultTwo", "businessResultThree")
-  
+  showBusinessResults()
+
   //should go to a business result
 }
 
+function showBusinessResults() {
+
+  if (document.getElementById("businessResultOne").style.visibility == "visible" &&
+      document.getElementById("businessResultTwo").style.visibility == "visible" &&
+      document.getElementById("businessResultThree").style.visibility="visible") {
+
+      document.getElementById("businessResultOne").style.visibility="hidden";
+      document.getElementById("businessResultTwo").style.visibility="hidden";
+  } else if (document.getElementById("businessResultOne").style.visibility == "visible") {
+  document.getElementById("businessResultTwo").style.visibility="visible";
+} else if (document.getElementById("businessResultOne").style.visibility != "visible") {
+  document.getElementById("businessResultOne").style.visibility="visible";
+}else if (document.getElementById("businessResultTwo").style.visibility == "visible") {
+  document.getElementById("businessResultThree").style.visibility="visible";
+}
+
 function artsButton() {
-  show("artResultOne", "artResultTwo")
-  
+  showArtsResults()
+
   //should go to an arts result
 }
 
+function showArtsResults() {
+  document.getElementById("artsResultOne").style.visibility="visible";
+  document.getElementById("artsResultTwo").style.visibility="visible";
+}
+
 function medButton() {
-  show("medResultOne", "medResultTwo")
-  
+  showMedResults()
+
   //should go to a med result
 }
 
+function showMedResults() {
+  document.getElementById("medResultOne").style.visibility="visible";
+  document.getElementById("medResultTwo").style.visibility="visible";
+}
+
 function undecidedButton() {
-  show("undecidedResultOne", "undecidedResultTwo")
-  
+  showUndecidedResults()
+
   //should go to an undecided result
+}
+
+function showUndecidedResults() {
+  document.getElementById("undecidedResultOne").style.visibility="visible";
+  document.getElementById("undecidedResultTwo").style.visibility="visible";
 }
